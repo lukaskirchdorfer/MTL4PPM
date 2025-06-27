fixed a bug for gradient computation

src/utils/trainer.py
@@ -180,7 +180,6 @@ class Trainer:
             self.optimizer.step()
             total_loss += loss.item()
             
-        print(task_weights_dic)
         # aggregate weights and gradients over all batches:
         if self.multi_task:
                 task_weights_dic = {
@@ -188,6 +187,7 @@ class Trainer:
                 gradient_dic = {
                     k: torch.mean(torch.stack(v), dim=0) 
                     for k, v in gradient_dic.items()}
+        print(task_weights_dic)
         
         # Calculate average losses and metrics
         num_batches = len(self.train_loader)

src/weighting/DWA.py
@@ -26,7 +26,8 @@ class DWA(AbsWeighting):
             batch_weight = self.task_num*F.softmax(w_i/T, dim=-1)
         else:
             batch_weight = torch.ones_like(losses).to(self.device)
-        grads = self._compute_grad(losses, mode='backward') # [task_num, grad_dim]
+        self._compute_grad_dim()
+        grads = self._compute_grad(losses, mode='autograd') # [task_num, grad_dim]
         loss = torch.mul(losses, batch_weight).sum()
         loss.backward()
         return batch_weight.detach().cpu().numpy(), grads

src/weighting/EW.py
@@ -15,7 +15,8 @@ class EW(AbsWeighting):
         super(EW, self).__init__()
         
     def backward(self, losses, **kwargs):
-        grads = self._compute_grad(losses, mode='backward') # [task_num, grad_dim]
+        self._compute_grad_dim()
+        grads = self._compute_grad(losses, mode='autograd') # [task_num, grad_dim]
         loss = torch.mul(losses, torch.ones_like(losses).to(self.device)).sum()
         loss.backward()
         return np.ones(len(losses)), grads

src/weighting/GradNorm.py
@@ -8,12 +8,14 @@ from src.weighting.abstract_weighting import AbsWeighting
 class GradNorm(AbsWeighting):
     r"""Gradient Normalization (GradNorm).
     
-    This method is proposed in `GradNorm: Gradient Normalization for Adaptive Loss Balancing in Deep Multitask Networks (ICML 2018) <http://proceedings.mlr.press/v80/chen18a/chen18a.pdf>`_ \
+    This method is proposed in `GradNorm: Gradient Normalization for 
+    Adaptive Loss Balancing in Deep Multitask Networks (ICML 2018) 
+    <http://proceedings.mlr.press/v80/chen18a/chen18a.pdf>`_ \
     and implemented by us.
 
     Args:
-        alpha (float, default=1.5): The strength of the restoring force which pulls tasks back to a common training rate.
-
+        alpha (float, default=1.5): The strength of the restoring force 
+        which pulls tasks back to a common training rate.
     """
     def __init__(self):
         super(GradNorm, self).__init__()
@@ -44,7 +46,8 @@ class GradNorm(AbsWeighting):
                 self._backward_new_grads(loss_weight, grads=grads)
             return loss_weight.cpu().numpy(), grads
         else:
-            grads = self._compute_grad(losses, mode='backward') # [task_num, grad_dim]
+            self._compute_grad_dim()
+            grads = self._compute_grad(losses, mode='autograd') # [task_num, grad_dim]
             loss = torch.mul(losses, torch.ones_like(losses).to(self.device)).sum()
             loss.backward()
             return np.ones(self.task_num), grads

src/weighting/RLW.py
@@ -17,7 +17,8 @@ class RLW(AbsWeighting):
         super(RLW, self).__init__()
         
     def backward(self, losses, **kwargs):
-        grads = self._compute_grad(losses, mode='backward') # [task_num, grad_dim]
+        self._compute_grad_dim()
+        grads = self._compute_grad(losses, mode='autograd') # [task_num, grad_dim]
         batch_weight = F.softmax(torch.randn(self.task_num), dim=-1).to(self.device)
         loss = torch.mul(losses, batch_weight).sum()
         loss.backward()

src/weighting/UW.py
@@ -19,6 +19,8 @@ class UW(AbsWeighting):
         self.loss_scale = nn.Parameter(torch.tensor([-0.5]*self.task_num, device=self.device))
         
     def backward(self, losses, **kwargs):
+        self._compute_grad_dim()
+        grads = self._compute_grad(losses, mode='autograd') # [task_num, grad_dim]
         loss = (losses/(2*self.loss_scale.exp())+self.loss_scale/2).sum()
         loss.backward()
-        return (1/(2*torch.exp(self.loss_scale))).detach().cpu().numpy()
+        return (1/(2*torch.exp(self.loss_scale))).detach().cpu().numpy(), grads

src/weighting/UW_SO.py
@@ -28,7 +28,8 @@ class UW_SO(AbsWeighting):
             batch_weight = 1 / (losses_detached + E)
         else:
             batch_weight = F.softmax((1 / (losses_detached + E)) / T, dim=-1)
-        grads = self._compute_grad(losses, mode='backward') # [task_num, grad_dim]
+        self._compute_grad_dim()
+        grads = self._compute_grad(losses, mode='autograd') # [task_num, grad_dim]
         loss = (batch_weight * losses).sum()
         loss.backward()
         return batch_weight.detach().cpu().numpy(), grads